82e92a770a74aac6e2667f075c37cdbac5bd5093
Andre-MR-Pereira <andregdpereira@gmail.com>
2020-10-20 14:21:06 +0100

    cathc input one time

--- functions.py
+++ functions.py
@@ -3,18 +3,21 @@ import random
 
 def generateSecret():
     passlist = ()
-    changer=list(passlist)
-    numberlist=list(range(1,9))
+    changer = list(passlist)
+    numberlist = list(range(1, 9))
     for i in range(4):
-        pick=random.randint(0,7-i)
+        pick = random.randint(0, 7-i)
         changer.append(numberlist.pop(pick))
-    passlist=tuple(changer)
+    passlist = tuple(changer)
     print(passlist)
     return passlist
 
+
 def generateInput():
-    answer=tuple()
-    changer=list(answer)
+    answer = tuple()
+    changer = list(answer)
+    valid = False
+    '''
     for i in range(4):
         repeat_flag=False
         entry=int(input('Please make your play:'))
@@ -27,25 +30,36 @@ def generateInput():
                 break
             entry=int(input('You cannot repeat numbers.Please make your play:'))
         changer.append(entry)
-    answer=tuple(changer)
+    '''
+    while valid == False:
+        valid = True
+        changer = [int(entry)
+                   for entry in input('Please make your play: ').split()]
+        for i in range(4):
+            if changer[i] > 8 or changer[i] < 1:
+                valid = False
+                print('Invalid entry. Numbers have to be between 1 and 8')
+
+    answer = tuple(changer)
     print(answer)
     return answer
-    
-def match(t,k):
-    placed_count=0
-    right_count=0
-    tchanger=list(t)
-    kchanger=list(k)
+
+
+def match(t, k):
+    placed_count = 0
+    right_count = 0
+    tchanger = list(t)
+    kchanger = list(k)
     for i in range(4):
-        if tchanger[i]==kchanger[i] :
-            placed_count+= 1
+        if tchanger[i] == kchanger[i]:
+            placed_count += 1
         for x in range(4):
-            if tchanger[i]==kchanger[x] :
-                right_count=right_count+1
-    
+            if tchanger[i] == kchanger[x]:
+                right_count = right_count+1
+
     clue = {
-        "placed" : placed_count ,
-        "right" : right_count
+        "placed": placed_count,
+        "right": right_count
     }
 
-    return clue   
+    return clue
--- main.py
+++ main.py
@@ -2,16 +2,19 @@ from functions import *
 from board import *
 from os import system
 
+
 def game():
     system('clear')
-    tabuleiro=Board()
+    tabuleiro = Board()
     tabuleiro.printBoard()
     for iteration in range(8):
-        attempt=generateInput()
-        info=tabuleiro.Guess(attempt,iteration)
+        attempt = generateInput()
+        info = tabuleiro.Guess(attempt, iteration)
         print(info)
-        if info==True or iteration==7:
+        if info == True or iteration == 7:
             tabuleiro.printResult(info)
             break
 
-game()
+
+if __name__ == "__main__":
+    game()
